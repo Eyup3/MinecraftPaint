e59efd8d16bfd4cd3d1e121e7caf09d2f1ed261b
Eyup3 <eyup.ozturk@outlook.de>
2021-05-23 15:59:09 +0200

    Finish Cleanup

--- WebServer.js
+++ WebServer.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("main")
+    res.sendFile(`${__dirname}\\public\\index.html`)
     console.log("Website served")
 })
 
